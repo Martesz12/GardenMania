dccbdf1a37919efb6059c68a3704565279e09d52
Bene Martin <h043098@stud.u-szeged.hu>
2022-04-25 18:57:30 +0200

    04.25. Profile

--- app/src/main/java/com/example/gardenmania/ProfileActivity.java
+++ app/src/main/java/com/example/gardenmania/ProfileActivity.java
@@ -4,25 +4,45 @@ import static android.view.View.GONE;
 import static android.view.View.VISIBLE;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.Image;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Base64;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.FrameLayout;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
+import java.io.ByteArrayOutputStream;
+
 public class ProfileActivity extends AppCompatActivity {
     private static final String LOG_TAG = ProfileActivity.class.getName();
     private static final String PREF_KEY = MainActivity.class.getPackage().toString();
+    // --------------- Profilkép készítéséhez szükséges kódok ---------------
+    private static final int REQUEST_CODE_ASK_PERMISSION = 420;
+    int tag = 1;
+    ImageView imageViewProfile;
+    Bundle pictureBundle;
+    Bitmap imgProfile;
+    String encoded;
     // ------------- Firebase autentikáció -------------
     private FirebaseUser user;
     // ------------- Menu kosár/kedvenc jelző -------------
@@ -51,9 +71,16 @@ public class ProfileActivity extends AppCompatActivity {
         preferences = getSharedPreferences(PREF_KEY, MODE_PRIVATE);
         if(preferences != null) {
             cartItems = preferences.getInt("cartItems", 0);
+            encoded = preferences.getString("profilePicture", "");
         }
         // ------------- Notification -------------
         mNotificationHandler = new NotificationHandler(this);
+        // --------------- Profilkép készítéséhez szükséges kódok ---------------
+        imageViewProfile = (ImageView)findViewById(R.id.imageViewProfile);
+        if(!encoded.equals("")){
+            byte[] imageAsBytes = Base64.decode(encoded.getBytes(), Base64.DEFAULT);
+            imageViewProfile.setImageBitmap(BitmapFactory.decodeByteArray(imageAsBytes, 0, imageAsBytes.length));
+        }
     }
 
 
@@ -156,13 +183,71 @@ public class ProfileActivity extends AppCompatActivity {
     }
 
     // --------------- Kosár dbszámának lementése ---------------
+    // --------------- Profilkép lementése és betöltése ---------------
     protected void onPause() {
         super.onPause();
 
+        if(imgProfile != null){
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            imgProfile.compress(Bitmap.CompressFormat.PNG, 100, baos); //bm is the bitmap object
+            byte[] b = baos.toByteArray();
+            encoded = Base64.encodeToString(b, Base64.DEFAULT);
+        }
+
         SharedPreferences.Editor editor = preferences.edit();
+        editor.putString("profilePicture", encoded);
         editor.putInt("cartItems", cartItems);
         editor.apply();
 
         Log.i(LOG_TAG, "onPause");
     }
+
+
+    // --------------- Profilkép készítése ---------------
+    public void openCamera(View view){
+        checkUserPermission();
+    }
+
+    void checkUserPermission(){
+        if(Build.VERSION.SDK_INT >= 23){
+            if(ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+                requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, REQUEST_CODE_ASK_PERMISSION);
+                return;
+            }
+        }
+        takePicture();
+    }
+
+    public void takePicture(){
+        Intent intent = new Intent();
+        intent.setAction(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent, tag);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode){
+            case REQUEST_CODE_ASK_PERMISSION:
+                if(grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    takePicture();
+                }else{
+                    Toast.makeText(this, "Kérés elutasítva!", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            default:
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == tag && resultCode == RESULT_OK){
+            pictureBundle = data.getExtras();
+            imgProfile = (Bitmap) pictureBundle.get("data");
+            imageViewProfile.setImageBitmap(imgProfile);
+        }
+    }
+
 }
